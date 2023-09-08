392b5bea82c53539a132ced04f58a3629afedeb7
Jakob Douglas <jakobdouglas1@gmail.com>
2023-09-08 15:10:31 +0930

    fix: added png converter to rest of application so all flags should now work

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -12,6 +12,14 @@ const formatDate = (date) =>
 		year: 'numeric',
 		weekday: 'long',
 	}).format(new Date(date));
+const flagemojiToPNG = (flag) => {
+	const countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+		.map((char) => String.fromCharCode(char - 127397).toLowerCase())
+		.join('');
+	return (
+		<img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt='flag' />
+	);
+};
 
 function City() {
 	const { id } = useParams();
@@ -29,7 +37,7 @@ function City() {
 			<div className={styles.row}>
 				<h6>City name</h6>
 				<h3>
-					<span>{emoji}</span> {cityName}
+					<span>{flagemojiToPNG(emoji)}</span> {cityName}
 				</h3>
 			</div>
 
